23a2011150297af34e57134b99860f4e1e511e3e
Olga Yurchenko <dlqpokupki@ukr.net>
2016-05-29 10:25:33 +0300

    first commit

---
+++ src/com/study/gojava/mod01/MyFirstProg.java
@@ -0,0 +1,8 @@
+package com.study.gojava.mod01;
+
+
+public class MyFirstProg {
+    public static void main(String[] args){
+        System.out.println("Hi, I'm your first java program! Stay tuned!");
+    }
+}
