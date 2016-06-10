a1c7e970ddf0aab3f24e97ed34948fc1a59bcba4
Olga Yurchenko <dlqpokupki@ukr.net>
2016-06-10 23:18:58 +0300

    third commit

--- src/com/study/gojava/mod05/SelectionSort.java
+++ src/com/study/gojava/mod05/SelectionSort.java
@@ -4,7 +4,7 @@ package com.study.gojava.mod05;
 public class SelectionSort extends ArrayOfNumbers {
 
     public void selectionSort(double[] arrayOfInt){
-        for (int i = 0; i < arrayOfInt.length; i++){
+        for (int i = 0; i < arrayOfInt.length-1; i++){
         double temp = arrayOfInt[i];
         arrayOfInt[i] = arrayOfInt[super.findMinElement(arrayOfInt)];
             arrayOfInt[super.findMaxElement(arrayOfInt)] = temp;
